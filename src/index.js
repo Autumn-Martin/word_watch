c05e06dc9e6a865a975e29ba39b95fe924e897ed
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-05 13:44:45 -0700

    Complete user story 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $('.word-count').html('');
+
+  fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
+    .then(response => response.json())
+    .then(data => {
+      $('.word-count').append(`
+        <p>${Object.keys(data.word)}: ${Object.values(data.word)}</p>`)
+    })
+  .catch(error => console.log({ error }))
+
 })
