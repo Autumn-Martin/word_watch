636c02fc50af30afb04c9c7f51fefe52863de7e3
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-05 15:21:43 -0700

    Refactor by extracting functions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import $ from 'jquery'
 $(document).ready(() => {
   $('.word-count').html('');
 
+  getTopWord()
+
+  $('.breakDownButton').click( () => {
+
+    let words = $('.inputField').val()
+    let wordsArr = words.split(" ")
+    $('.breakDance').html('')
+
+    addWords(wordsArr)
+  })
+
+})
+
+const getTopWord = () => {
   fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
     .then(response => response.json())
     .then(data => {
@@ -10,26 +24,22 @@ $(document).ready(() => {
         <p>${Object.keys(data.word)}: ${Object.values(data.word)}</p>`)
     })
   .catch(error => console.log({ error }))
+}
 
-  $('.breakDownButton').click( () => {
-    let words = $('.inputField').val()
-    let wordsArr = words.split(" ")
-    $('.breakDance').html('')
-    for (let word of wordsArr) {
-      const pack = {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({
-          word: { value: word }
-        }),
-      }
-      fetch(`https://wordwatch-api.herokuapp.com/api/v1/words`, pack)
-        .then(response => response.json())
-        .then(data => {
-
-          $('.breakDance').append(`</br><p>${data["message"]}</p>`)
-        })
+const addWords = (wordsArr) => {
+  for (let word of wordsArr) {
+    const pack = {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        word: { value: word }
+      }),
     }
-  })
+    fetch(`https://wordwatch-api.herokuapp.com/api/v1/words`, pack)
+      .then(response => response.json())
+      .then(data => {
 
-})
+        $('.breakDance').append(`</br><p>${data["message"]}</p>`)
+      })
+  }
+}
